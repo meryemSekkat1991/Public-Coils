bf5167cf9a00401c028ffc5c2a404de9a3720179
meryem sekkat <34552218+meryemSekkat1991@users.noreply.github.com>
2021-10-05 15:46:57 +0200

    feat: products search (#5)

--- pages/Products.vue
+++ pages/Products.vue
@@ -1,69 +1,119 @@
 <template lang="pug">
-.products
-  table.table.table-hover.w-full
-    thead
-      tr
-        th b_800
-        th cnt
-        th code
-        th grade
-        th health_state
-        th id
-        th info
-        th perte
-        th ref
-        th reference
-        th sc
-        th state
-        th th
-        th warehouse
-        th warehouse_id
-        th weight_brut: 0
-        th weight_net: 1105
-        th width
-    tbody
-      tr(v-for='(item, index) in products' :key="'item-' + index" v-bind:class="{'active': index == 2}" class="hover")
-        th {{item.b_800}}
-        th {{item.cnt}}
-        th {{item.code}}
-        th {{item.grade}}
-        th {{item.health_state}}
-        th {{item.id}}
-        th {{item.info}}
-        th {{item.perte}}
-        th {{item.ref}}
-        th {{item.reference}}
-        th {{item.sc}}
-        th {{item.state}}
-        th {{item.th}}
-        th {{item.warehouse}}
-        th {{item.warehouse_id}}
-        th {{item.weight_brut}}
-        th {{item.weight_net}}
-        th {{item.width}}
-
+  .products
+    .form-control
+      label.label
+        span.label-text.font-bold Search By name
+      .flex.space-x-2.mb-4
+        input.w-full.input.input-primary.input-bordered(type='text' placeholder='Search' v-model="params.q" @input="createQuery")
+    table.table.table-hover.w-full
+      thead
+        tr
+          th id
+          th b_800
+          th code
+          th grade
+          th health_state
+          th perte
+          th ref
+          th reference
+          th sc
+          th state
+          th th
+          th warehouse
+          th warehouseId
+          th brut
+          th net
+          th width
+      tbody
+        tr(v-for='(item, index) in products' :key="'item-' + index" class="hover")
+          th {{item.id}}
+          th {{item.b_800}}
+          th {{item.code}}
+          th {{item.grade}}
+          th {{item.health_state}}
+          th {{item.perte}}
+          th {{item.ref}}
+          th {{item.reference}}
+          th {{item.sc}}
+          th {{item.state}}
+          th {{item.th}}
+          th {{item.warehouse}}
+          th {{item.warehouse_id}}
+          th {{item.weight_brut}}
+          th {{item.weight_net}}
+          th {{item.width}}
+    .flex.justify-center.my-4
+      Pagination(:meta="pagination" classes="btn-md bg-indigo-600 border-none hover:bg-indigo-900" @page-changed="pageChanged")
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "nuxt-property-decorator";
+  import { Component, Vue, Watch } from "nuxt-property-decorator";
+  import { isEmpty, set, toSafeInteger } from "lodash";
 
-@Component({
-  name: "Products",
-  layout: "default",
-})
+  @Component({
+    name: "Products",
+    layout: "default",
+    watch: {
+      '$route.query': '$fetch'
+    },
+  })
 
-export default class Products extends Vue {
-  products = [];
-  async fetch (): Promise<void> {
-    await this.$axios
-      .get('/api/admin/slitters')
-      .then(res => (
-        this.products = res.data.data
-      ));
+  export default class Products extends Vue {
+    products = [];
+    currentPage = 1;
+    params = {
+      q: "",
+      page: 1,
+    };
+
+    pagination = {
+      current_page: 1,
+      last_page: 15,
+      next_page: 2,
+      prev_page: 1,
+      total_count: 100,
+      per_page: 9
+    }
+
+
+    createQuery(): void {
+      const query = {
+        page: this.currentPage > 1 ? this.currentPage.toString() : undefined,
+        q: this.params.q || undefined,
+      };
+
+      this.$router.push({
+        name: this.$route.name!,
+        query,
+      });
+    }
+
+    async fetch (): Promise<void> {
+      await this.$axios
+        .get('/api/admin/slitters?', {
+          params: {
+            search: this.params.q,
+            page_size: 100,
+            page: this.currentPage
+          }
+        })
+        .then(res => (
+          this.products = res.data.data
+        ));
+    }
+
+    pageChanged(page: number): void {
+      this.currentPage = page;
+      this.createQuery();
+    }
+
+    @Watch("$route", { immediate: true })
+    setQuery(): void {
+      this.params = {
+        q: this.$route.query.q?.toString() || "",
+        page: this.currentPage
+      };
+      this.currentPage = toSafeInteger(this.$route.query.page) || 1;
+    }
   }
-}
 </script>
-
-<style lang="scss">
-
-</style>
